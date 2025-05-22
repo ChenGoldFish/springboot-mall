dfcaba8260a2920c63d0ca89e53ffb93593657c9
ChenGoldFish <vsop02092@gmail.com>
2025-05-22 22:57:10 +0800

    商品功能—實作查詢商品列表(基礎實作)

--- springboot-mall/src/main/java/com/chengoldfish/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/chengoldfish/springbootmall/service/ProductService.java
@@ -3,8 +3,11 @@ package com.chengoldfish.springbootmall.service;
 import com.chengoldfish.springbootmall.dto.ProductRequest;
 import com.chengoldfish.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
 
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
