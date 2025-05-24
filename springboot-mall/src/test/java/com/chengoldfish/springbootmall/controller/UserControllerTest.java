845653d6afb766dc2d379eea7ba2b47a40ce6045
ChenGoldFish <vsop02092@gmail.com>
2025-05-24 22:42:58 +0800

    帳號功能-單元測試(5/24待測試解決)

---
+++ springboot-mall/src/test/java/com/chengoldfish/springbootmall/controller/UserControllerTest.java
@@ -0,0 +1,61 @@
+package com.chengoldfish.springbootmall.controller;
+
+import com.chengoldfish.springbootmall.dao.UserDAO;
+import com.chengoldfish.springbootmall.dto.UserRegisterRequest;
+import com.chengoldfish.springbootmall.model.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hibernate.validator.internal.constraintvalidators.bv.NotNullValidator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserDAO userDAO;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    //註冊新帳號
+    @Test
+    public void register_success() throws Exception {
+        UserRegisterRequest userRegisterRequest = new UserRegisterRequest();
+        userRegisterRequest.setEmail("test1@gmail.com");
+        userRegisterRequest.setPassword("123");
+
+        String json = objectMapper.writeValueAsString(userRegisterRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/user/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(201))
+                .andExpect(jsonPath("$.userId", notNullValue()))
+                .andExpect(jsonPath("$.email", equalTo("test1@gmail.com")))
+                .andExpect(jsonPath("$.createdDate", notNullValue()))
+                .andExpect(jsonPath("$.lastModifiedDate", notNullValue()));
+
+        // 檢查資料庫中的密碼不為明碼
+        User user = userDAO.getUserByEmail(userRegisterRequest.getEmail());
+        assertNotEquals(userRegisterRequest.getPassword(), user.getPassword());
+    }
+}
