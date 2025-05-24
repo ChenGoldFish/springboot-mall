52236c1c1d2bd95270ae98f36ffdccfaa92f32d4
ChenGoldFish <vsop02092@gmail.com>
2025-05-24 14:51:41 +0800

    商品功能—單元測試

---
+++ springboot-mall/src/test/java/com/chengoldfish/springbootmall/controller/ProductControllerTest.java
@@ -0,0 +1,42 @@
+package com.chengoldfish.springbootmall.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.chengoldfish.springbootmall.constant.ProductCategory;
+import com.chengoldfish.springbootmall.dto.ProductRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    // 查詢商品
+    @Test
+    public void getProduct_success() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/products/{productId}", 1);
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.productName", equalTo("蘋果（澳洲）")));
+    }
+}
